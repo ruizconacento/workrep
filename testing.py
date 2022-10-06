67dc0a4fcbe7c8df2733ffb0c97c7ef4741521a6
Miguel Ruiz <miguel.ar.997@gmail.com>
2022-10-06 08:41:14 -0500

    Yesterday, I was remember some things about python,  I leave the code on projects

--- testing.py
+++ testing.py
@@ -19,7 +19,14 @@ for item in main_path_:
         print(item_)
         break
     
-#data_xls = pd.read_excel(item_, "Sheet1")
+data_xls = pd.read_excel(current_path, index_col=0)
+data = data_xls.to_csv(file + ".csv",encoding='utf-8', index=False)
+
+data_f = pd.read_csv(data)
+
+
+
+
 
 
 
