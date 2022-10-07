98c109435ffbc0962226442ae05462281ad3168d
Miguel Ruiz <miguel.ar.997@gmail.com>
2022-10-07 17:53:59 -0500

    Finally, I was bring information from two sources

--- testing.py
+++ testing.py
@@ -1,4 +1,3 @@
-from datetime import date
 from datetime import datetime
 from xml.etree.ElementTree import tostring
 import pandas as pd
@@ -9,14 +8,21 @@ file = "OT " + datetime.now().strftime('%d.%m.%Y') + ".XLSX"
 main_path = "C:\\Users\\mruiz\\Documents\\DB OT" 
 current_month = datetime.now().strftime('%B')
 main_path_ = os.listdir(main_path)
+#print(main_path_)
 #Then, we need to find the correct file
 for item in main_path_:
     if item == file:
-        item_ = item
-        print(item_)
+        item1 = item
+        print(item1)
+        break
+for item in main_path_:
+    if item == 'Master_OT.xlsx':
+        item2 = item
+        print(item2)
         break
 #Then, just open it to start    
-destination_path = main_path + "\\" + item_
-data = pd.read_excel(destination_path, index_col=0, sheet_name="Sheet1", header=0)
-print(data.info())
-print(data)
+data1 = main_path + "\\" + item1
+data2 = main_path + "\\" + item2
+data1_ = pd.read_excel(data1, index_col=0, sheet_name="Sheet1", header=0)
+data2_ = pd.read_excel(data2, index_col=0, sheet_name="Names", header=0)
+print(data2_.to_string())
