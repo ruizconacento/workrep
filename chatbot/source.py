f2dcd49355c622b05decff027723b4cee51c3b0c
Miguel Ruiz <miguel.ar.997@gmail.com>
2022-11-08 14:49:10 -0600

    I unrestricted Scripts could run in powershell

--- chatbot/source.py
+++ chatbot/source.py
@@ -1,7 +1,20 @@
+import unittest
+from pyunitreport import HTMLTestRunner
 from selenium import webdriver
-import time
-driver = webdriver.Chrome()
-driver.get("http://google.com")
-time.sleep(2)
-driver.quit()
-print("now!")
+
+class HellowWorld(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path = r'C:\Users\mruiz\Documents\pythonscripts\chatbot\chromedriver_win32\chromedriver.exe')
+        driver = self.driver
+        driver.implicitly_wait(10)
+
+    def test_hello_world(self):
+        driver = self.driver
+        driver.get('http://www.platzi.com')
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main(verbosity = 2, testRunner = HTMLTestRunner(output = 'reportes',report_name = 'hello-world-report'))
